61db98b83f5845e1e0a7fd2a527df42c4d229cf2
rishikant42 <rksharma@karix.com>
2018-12-25 12:53:13 +0530

    hello world app

---
+++ hello-world/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2> Hello {{ name }} </h2>
+    <p>{{ gretting() }}</p>
+    <HelloWorld msg="Hello world"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  data() {
+    return {
+      name: 'Rishi',
+    }
+  },
+
+  methods: {
+    gretting() {
+      return "Welcome to vuejs tutorial"
+    }
+  },
+
+  components: {
+    HelloWorld: HelloWorld
+  }
+}
+</script>
