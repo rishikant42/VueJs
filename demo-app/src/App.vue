774da24e45609a7fd1110872921ec0891c69ce56
rishikant42 <rksharma@karix.com>
2018-12-25 16:35:50 +0530

    sub componentys

---
+++ demo-app/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <app-header/>
+    <app-ninjas/>
+    <app-footer/>
+  </div>
+</template>
+
+<script>
+
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+import Ninjas from "@/components/Ninjas.vue";
+
+export default {
+  components: {
+    'app-header': Header,
+    'app-footer': Footer,
+    'app-ninjas': Ninjas,
+  },
+
+  data() {
+    return {
+    }
+  },
+}
+
+</script>
+
+<style>
+
+</style>
