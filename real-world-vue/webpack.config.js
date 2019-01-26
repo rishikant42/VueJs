8a8cd661a8ce4c31a64a73cf341a8f46396c118a
rishikant42 <rksharma@karix.com>
2019-01-26 15:26:42 +0530

    webpack conf

---
+++ real-world-vue/webpack.config.js
@@ -0,0 +1,11 @@
+let webpack = require('webpack');
+let path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: './dist'
+  }
+};
---
+++ webpack-test/src/index.js
@@ -0,0 +1,8 @@
+alert("Hello world");
+
+
+const app = (a, b) => {
+   return a + b;
+}
+
+alert(app(4,5));
