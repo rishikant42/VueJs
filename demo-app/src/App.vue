5ad14f9c6217adebe90ca02a34d2d93651107c18
rishikant42 <rksharma@karix.com>
2019-02-15 14:01:16 +0530

    git history

--- demo-app/src/App.vue
+++ demo-app/src/App.vue
@@ -22,12 +22,11 @@ export default {
   data() {
     return {
       ninjas: [
-        {name: 'Ryu', speciality: 'Vue Components', show: false},
         {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
         {name: 'Hitoshi', speciality: 'Click Events', show: false},
         {name: 'Tango', speciality: 'Conditionals', show: false},
         {name: 'Kami', speciality: 'Webpack', show: false},
-        {name: 'Yoshi', speciality: 'Data Diggin', show: false}
+        {name: 'GIT', speciality: 'Historu', show: false}
       ],
 
       title: 'Vue Ninjas',
