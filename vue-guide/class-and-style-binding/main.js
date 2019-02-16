08383030717e9604a0c8ffb76f1c95d392b9c800
rishikant42 <rksharma@karix.com>
2019-02-16 14:55:51 +0530

    class bind

---
+++ vue-guide/class-and-style-binding/main.js
@@ -0,0 +1,17 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    classObj: {active: true},
+    cls: {'text-danger': true},
+    message: 'Hello world',
+    // activeClass: 'active',
+    // errorClass: 'text-danger',
+    // isActive: true,
+    // styleObj: {
+    //   color: 'green',
+    // },
+    // stylenewObj: {
+    //   'font-size': '50px'
+    // },
+  }
+})
